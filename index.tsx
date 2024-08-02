ad5f4dc8d550d053bef70609c8253df50374fb09
PANKAJ KUMAR PRAJAPATI <pkp@Pankaj-Kumar.local>
2024-08-02 20:36:17 +0530

    Extra Key Search

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import {
   FlatList,
   Modal,
@@ -6,20 +6,20 @@ import {
   StyleSheet,
   Text,
   View,
-} from 'react-native';
-import Buttons from './components/button';
-import {CheckBox} from './components/checkbox';
-import SearchBar from './components/search-bar';
-import {PickerProps} from './utils/props-type';
-import {COLORS} from './utils/values';
+} from "react-native";
+import Buttons from "./components/button";
+import { CheckBox } from "./components/checkbox";
+import SearchBar from "./components/search-bar";
+import { PickerProps } from "./utils/props-type";
+import { COLORS } from "./utils/values";
 
 const MultipleSelection: React.FC<PickerProps> = ({
   show,
   type,
   enableSearch = true,
-  searchPlaceholder = 'Search here',
+  searchPlaceholder = "Search here",
   pickerTitle,
-  emptyTitle = 'No Record(s) Found',
+  emptyTitle = "No Record(s) Found",
   data,
   value,
   rowTitleKey,
@@ -32,14 +32,14 @@ const MultipleSelection: React.FC<PickerProps> = ({
 }) => {
   const [selectedData, setSelectedData] = useState<any>([]);
   const [searchData, setSearchData] = useState([]);
-  const [searchText, setSearchText] = useState<string>('');
+  const [searchText, setSearchText] = useState<string>("");
   const [allSelected, setAllSelected] = useState<boolean>(false);
   const listRef = React.useRef(null);
   useEffect(() => {
-    if (value !== '' && show && type === 'single') {
+    if (value !== "" && show && type === "single") {
       setSelectedData(value);
       const index = data?.findIndex(
-        item => item[rowUniqueKey] == value[rowUniqueKey],
+        (item) => item[rowUniqueKey] == value[rowUniqueKey]
       );
       if (show && index > -1) {
         listRef.current?.scrollToIndex({
@@ -49,15 +49,15 @@ const MultipleSelection: React.FC<PickerProps> = ({
         });
       }
     } else if (
-      value !== '' &&
+      value !== "" &&
       show &&
-      type === 'multiple' &&
+      type === "multiple" &&
       value?.length > 0
     ) {
       setSelectedData(
-        data?.filter(e =>
-          value?.some(item => item[rowUniqueKey] === e[rowUniqueKey]),
-        ),
+        data?.filter((e) =>
+          value?.some((item) => item[rowUniqueKey] === e[rowUniqueKey])
+        )
       );
     }
     if (selectedData?.length === data?.length) {
@@ -66,7 +66,7 @@ const MultipleSelection: React.FC<PickerProps> = ({
       setAllSelected(false);
     }
     return () => {
-      setSearchText('');
+      setSearchText("");
       setSearchData([]);
     };
   }, [show, value]);
@@ -82,20 +82,18 @@ const MultipleSelection: React.FC<PickerProps> = ({
   return React.useMemo(() => {
     const onChangeText = (txt: string) => {
       setSearchData(
-        data.filter(item => {
+        data.filter((item) => {
           if (extraTitleKey) {
             return (
               item[rowTitleKey]?.toLowerCase()?.includes(txt?.toLowerCase()) ||
-              item[extraTitleSymbol]
-                ?.toLowerCase()
-                ?.includes(txt?.toLowerCase())
+              item[extraTitleKey]?.toLowerCase()?.includes(txt?.toLowerCase())
             );
           } else {
             return item[rowTitleKey]
               ?.toLowerCase()
               ?.includes(txt?.toLowerCase());
           }
-        }),
+        })
       );
       setSearchText(txt);
     };
@@ -123,12 +121,12 @@ const MultipleSelection: React.FC<PickerProps> = ({
     };
     const onItemPress = (item: any, isAdded: boolean) => {
       let allData = [...selectedData];
-      if (type === 'single') {
+      if (type === "single") {
         setSelectedData([item]);
       } else {
         if (isAdded) {
           allData = allData.filter(
-            el => el[rowUniqueKey] !== item[rowUniqueKey],
+            (el) => el[rowUniqueKey] !== item[rowUniqueKey]
           );
         } else {
           allData.push(item);
@@ -143,34 +141,36 @@ const MultipleSelection: React.FC<PickerProps> = ({
       }
     };
 
-    const showListData = searchText === '' ? data : searchData;
+    const showListData = searchText === "" ? data : searchData;
     return (
       <Modal
         visible={show}
-        animationType={'slide'}
+        animationType={"slide"}
         transparent={true}
-        onRequestClose={onClose}>
+        onRequestClose={onClose}
+      >
         <SafeAreaView style={styles.safearea}>
           <View style={styles.container}>
             <View style={styles.innerContainer}>
               <View style={styles.buttonsRow}>
                 <Buttons
-                  title={'Cancel'}
+                  title={"Cancel"}
                   onPress={onClosed}
-                  type={'close'}
+                  type={"close"}
                   pickerColor={pickerColor}
                 />
                 <Text
                   style={[
                     styles.pickerTitleText,
-                    pickerColor && {color: pickerColor},
-                  ]}>
+                    pickerColor && { color: pickerColor },
+                  ]}
+                >
                   {pickerTitle}
                 </Text>
                 <Buttons
-                  title={'Done'}
+                  title={"Done"}
                   onPress={onDonePress}
-                  type={'done'}
+                  type={"done"}
                   pickerColor={pickerColor}
                 />
               </View>
@@ -180,17 +180,17 @@ const MultipleSelection: React.FC<PickerProps> = ({
                   value={searchText}
                   placeholder={searchPlaceholder}
                   onChangeText={onChangeText}
-                  onClear={() => setSearchText('')}
+                  onClear={() => setSearchText("")}
                 />
               )}
 
               <View>
-                {type === 'multiple' && showListData?.length > 0 && (
+                {type === "multiple" && showListData?.length > 0 && (
                   <>
                     <CheckBox
                       onPress={onSelectAllPress}
                       isChecked={allSelected}
-                      title={'Select All'}
+                      title={"Select All"}
                       pickerColor={pickerColor}
                     />
 
@@ -203,7 +203,7 @@ const MultipleSelection: React.FC<PickerProps> = ({
                   ref={listRef}
                   data={showListData}
                   // style={{flex: 1}}
-                  contentContainerStyle={{paddingBottom: 100}}
+                  contentContainerStyle={{ paddingBottom: 100 }}
                   ListEmptyComponent={() => {
                     return (
                       <View style={styles.emptyView}>
@@ -211,9 +211,9 @@ const MultipleSelection: React.FC<PickerProps> = ({
                       </View>
                     );
                   }}
-                  renderItem={({item}) => {
+                  renderItem={({ item }) => {
                     const active = selectedData?.some(
-                      e => e[rowUniqueKey] === item[rowUniqueKey],
+                      (e) => e[rowUniqueKey] === item[rowUniqueKey]
                     );
                     return (
                       <React.Fragment key={item[rowUniqueKey]}>
@@ -234,10 +234,10 @@ const MultipleSelection: React.FC<PickerProps> = ({
                   viewabilityConfig={{
                     itemVisiblePercentThreshold: 50, // Adjust as needed
                   }}
-                  onScrollToIndexFailed={info => {
-                    if (type === 'single') {
-                      const wait = new Promise(resolve =>
-                        setTimeout(resolve, 500),
+                  onScrollToIndexFailed={(info) => {
+                    if (type === "single") {
+                      const wait = new Promise((resolve) =>
+                        setTimeout(resolve, 500)
                       );
                       wait.then(() => {
                         listRef.current?.scrollToIndex({
@@ -248,7 +248,7 @@ const MultipleSelection: React.FC<PickerProps> = ({
                       });
                     }
                   }}
-                  keyExtractor={item => item[rowUniqueKey]}
+                  keyExtractor={(item) => item[rowUniqueKey]}
                 />
               </View>
             </View>
@@ -278,33 +278,33 @@ const MultipleSelection: React.FC<PickerProps> = ({
 const styles = StyleSheet.create({
   emptyView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 110,
   },
   emptyTitleText: {
     fontSize: 20,
-    fontWeight: '500',
-    textAlign: 'center',
+    fontWeight: "500",
+    textAlign: "center",
     color: COLORS.TITLE,
   },
 
-  listView: {marginBottom: 110},
+  listView: { marginBottom: 110 },
   devider: {
     height: 1,
     backgroundColor: COLORS.DEVIDER,
   },
   buttonsRow: {
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: COLORS.WHITE,
   },
   pickerTitleText: {
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     color: COLORS.PRIMARY,
     flex: 1,
   },
@@ -318,13 +318,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.RGBA,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
   innerContainer: {
     backgroundColor: COLORS.WHITE,
     paddingVertical: 15,
     paddingHorizontal: 5,
-    maxHeight: '80%',
+    maxHeight: "80%",
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
   },
